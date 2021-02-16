0a8646d4575aa38f58622be8fa91a82d1c3e196a
Cyklan <37187056+Cyklan@users.noreply.github.com>
2021-02-16 19:47:50 +0100

    Menu State

---
+++ Brackeys/States/MenuState.Components.cs
@@ -0,0 +1,33 @@
+﻿using Brackeys.Component.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Brackeys.States
+{
+    public partial class MenuState : State
+    {
+        protected override void LoadComponents()
+        {
+            AddUi();
+        }
+
+        private void AddUi()
+        {
+            Button startGameButton = new Button(ContentManager.RangeTexture, ContentManager.TestFont)
+            {
+                Text = "Start Game",
+                Position = new Vector2(JamGame.ScaleOriginal.Width / 2 - 50, JamGame.ScaleOriginal.Height / 2 - 20),
+                Size = new System.Drawing.Size(100, 40)
+            };
+
+            startGameButton.OnClick += (sender, e) =>
+            {
+                StateManager.ChangeTo<GameState>(GameState.Name);
+            };
+
+            AddComponent(startGameButton, States.Layers.UI);
+        }
+    }
+}
